8f1cfe11d15b3cc3f189e30b2f1510e98f5789e9
shammi <shammikumar3833@gmail.com>
2026-03-15 17:45:07 +0530

    Hyperparameter training

--- src/utils.py
+++ src/utils.py
@@ -16,7 +16,7 @@ from src.exception import CustomException
 #for model training
 #evaluation metrics 
 from sklearn.metrics import r2_score
-
+from sklearn.model_selection import GridSearchCV
 
 
 def save_object(file_path, obj):
@@ -28,43 +28,68 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomException(e, sys)
-    
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+
+# method 2
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
         for model_name, model in models.items():
-            model.fit(X_train, y_train)
+                para=params[model_name]
+    
+                # hyperparameter tuning
+                gs = GridSearchCV(model, para, cv=3)
+                gs.fit(X_train, y_train)
+    
+                # update model with best parameters
+                model.set_params(**gs.best_params_)
+                # train final model
+                model.fit(X_train, y_train)
+                # predict training data
+                y_train_pred = model.predict(X_train)
+                # predict testing data
+                y_test_pred = model.predict(X_test)
+                # train and test model score
+                train_model_score = r2_score(y_train, y_train_pred)
+                test_model_score = r2_score(y_test, y_test_pred)
+                # save the best model score in report dictionary
+                report[model_name] = test_model_score
 
-            y_train_pred = model.predict(X_train)
-            y_test_pred = model.predict(X_test)
-
-            train_model_score = r2_score(y_train, y_train_pred)
-            test_model_score = r2_score(y_test, y_test_pred)
-
-            report[model_name] = test_model_score
-
-        return report
+                return report
 
     except Exception as e:
         raise CustomException(e, sys)   
-    
-# def evalute_model(model, X_train, y_train, X_test, y_test):
+
+#method 1
+
+# def evalute_model(model, X_train, y_train, X_test, y_test,models,params):
 #     try:
 #         report = {}
        
 #         for i in range(len(list(model))):
+           
 #             model = list(model.values())[i]
+#             para=params[list(model.keys())[i]]
+            
+#             # hyperparameter tuning
+#             gs = GridSearchCV(model, para, cv=3)
+#             gs.fit(X_train, y_train)
+
+#             # update model with best parameters
+#             model.set_params(**gs.best_params_)
+#             # train final model
 #             model.fit(X_train, y_train)
-
+#             # predict training data
 #             y_train_pred = model.predict(X_train)
+#             # predict testing data
 #             y_test_pred = model.predict(X_test)
-
+#             #train and test model score
 #             train_model_score = r2_score(y_train, y_train_pred)
 #             test_model_score = r2_score(y_test, y_test_pred)
-
+#         # save the best model score in report dictionary
 #         report[list(model.keys())[i]] = test_model_score
       
 #         return report
 
-#     except Exception as e:
-#         raise CustomException(e, sys)
+    except Exception as e:
+        raise CustomException(e, sys)
+
